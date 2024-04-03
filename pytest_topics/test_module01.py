f47e10959b68c68fa73f311343422587462fe16f
Mohammad Ghaffary <m.ghaffari662@gmail.com>
2024-04-03 09:39:27 +0330

    firt script

---
+++ pytest_topics/test_module01.py
@@ -0,0 +1,13 @@
+def test_a1():
+    assert 5 + 5 == 10
+    assert 5 - 5 == 0
+
+
+# this is a failed test
+def test_a2():
+    assert 9.5 == 1.5, "failed test intentionally"
+
+
+def test_a3():
+    assert 9//5 == 1
+
